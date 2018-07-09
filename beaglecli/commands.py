494aef998da8e421c481af51da01d64250a8d893
Marco Marzetti <marco@lamehost.it>
2018-07-09 20:06:21 +0200

    added common function to print command results

--- beaglecli/commands.py
+++ beaglecli/commands.py
@@ -35,78 +35,108 @@ import sys
 import click
 
 
+def _print_result(result):
+    # TODO: Find a library for this
+    try:
+        _  = result['body']['data']['output']
+        _ = _.replace('<b>', '\033[1m')
+        _ = _.replace('</b>', '\033[0m')
+        result['body']['data']['output'] = _
+    except KeyError:
+        pass
+
+    if result['status'] in [200, 202]:
+        print('\033[1mStatus\033[0m: %(status)s' % result['body'])
+        print('\033[1mTimestamp\033[0m: %(performed_at)s' % result['body']['data'])
+        print('\n\033[1mOutput\033[0m:\n%(output)s' % result['body']['data'])
+    elif result['status'] in [400, 404]:
+        print('Server didn\'t like the argument. Please try with something meaningful.')
+        sys.exit(1)
+    elif result['status'] in [429]:
+        print(u'(╯°□°）╯︵ ┻━┻ Hey, what are you up to? Slow down!')
+        sys.exit(1)
+    elif result['status'] in [502]:
+        print('\033[1mStatus\033[0m: %(status)s' % result['body'])
+        print('\n\033[1mOutput\033[0m:\n%(message)s\n' % result['body']['data'])
+    elif result['status'] in [504]:
+        print(u'(╯°□°）╯︵ ┻━┻ Command timed out on the router')
+    else:
+        print(u'(╯°□°）╯︵ ┻━┻ Something went wrong with the server')
+
+    print('')
+
 def ping(router, address, vrf, max_routers, afi):
     ctx = click.get_current_context()
     try:
         routers = ctx.meta['CLIENT'].get_routers(router)
     except RuntimeError, error:
-        click.echo(error)
+        print(error)
         sys.exit(1)
 
     limit = len(routers) if len(routers) < max_routers else max_routers
-    click.echo(
-        'Routers: %s\n' % ', '.join(sorted([_['name'] for _ in routers[:limit]]))
-    )
+    _ = ', '.join(sorted([_['name'] for _ in routers[:limit]]))
+    print('\033[1mSelected routers\033[0m: %s\n' % _)
 
     for _router in routers[:limit]:
-        click.echo('Running query on router: %s' % _router['name'])
+        print('\033[1mRouter\033[0m: %s' % _router['name'])
         try:
-            output = ctx.meta['CLIENT'].ping(address, _router['id'], afi, vrf)
+            result = ctx.meta['CLIENT'].ping(address, _router['id'], afi, vrf)
         except RuntimeError, error:
-            output = error
+            print(error)
+            sys.exit(1)
 
-        click.echo('%s\n' % output)
+        _print_result(result)
 
 def show_bgp_unicast(router, prefix, vrf, max_routers, afi):
     ctx = click.get_current_context()
     try:
         routers = ctx.meta['CLIENT'].get_routers(router)
     except RuntimeError, error:
-        click.echo(error)
+        print(error)
         sys.exit(1)
 
     limit = len(routers) if len(routers) < max_routers else max_routers
-    click.echo(
-        'Routers: %s\n' % ', '.join(sorted([_['name'] for _ in routers[:limit]]))
-    )
+    _ = ', '.join(sorted([_['name'] for _ in routers[:limit]]))
+    print('\033[1mSelected routers\033[0m: %s\n' % _)
 
     for _router in routers[:limit]:
-        click.echo('Running query on router: %s' % _router['name'])
+        print('\033[1mRouter\033[0m: %s' % _router['name'])
         try:
-            output = ctx.meta['CLIENT'].show_bgp_unicast(prefix, _router['id'], afi, vrf)
+            result = ctx.meta['CLIENT'].show_bgp_unicast(prefix, _router['id'], afi, vrf)
         except RuntimeError, error:
-            output = error
+            print(error)
+            sys.exit(1)
 
-        click.echo('%s\n' % output)
+        _print_result(result)
 
 def show_routers(router):
     ctx = click.get_current_context()
     try:
-        output = ctx.meta['CLIENT'].show_routers(router)
+        result = ctx.meta['CLIENT'].show_routers(router)
     except RuntimeError, error:
-        click.echo(error)
+        print(error)
         sys.exit(1)
 
-    click.echo(output)
+    print(result)
 
 def traceroute(router, address, vrf, max_routers, afi):
     ctx = click.get_current_context()
     try:
         routers = ctx.meta['CLIENT'].get_routers(router)
     except RuntimeError, error:
-        click.echo(error)
+        print(error)
         sys.exit(1)
 
     limit = len(routers) if len(routers) < max_routers else max_routers
-    click.echo(
-        'Routers: %s\n' % ', '.join(sorted([_['name'] for _ in routers[:limit]]))
-    )
+    _ = ', '.join(sorted([_['name'] for _ in routers[:limit]]))
+    print('\033[1mSelected routers\033[0m: %s\n' % _)
 
     for _router in routers[:limit]:
-        click.echo('Running query on router: %s' % _router['name'])
+        print('\033[1mRouter\033[0m: %s' % _router['name'])
         try:
-            output = ctx.meta['CLIENT'].traceroute(address, _router['id'], afi, vrf)
+            result = ctx.meta['CLIENT'].traceroute(address, _router['id'], afi, vrf)
         except RuntimeError, error:
-            output = error
+            print(error)
+            sys.exit(1)
 
-        click.echo('%s\n' % output)
+        _print_result(result)
