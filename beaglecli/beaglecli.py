6068f95331b5f3938992a1bbacd909e6c9894a94
Marco Marzetti <marco@lamehost.it>
2018-07-31 17:51:57 +0200

    lot lot of changes

--- beaglecli/beaglecli.py
+++ beaglecli/beaglecli.py
@@ -32,111 +32,117 @@ from __future__ import print_function
 
 import sys
 
-import click
+from beaglecli.rest_client import RESTClient
 
 
-def _print_result(result):
-    # TODO: Find a library for this
-    try:
-        _  = result['body']['data']['output']
-        _ = _.replace('<b>', '\033[1m')
-        _ = _.replace('</b>', '\033[0m')
-        result['body']['data']['output'] = _
-    except KeyError:
-        pass
+class Commands(object):
+    def __init__(self, api_base, username, password, verify_ssl, debug):
+        self.client = RESTClient(
+            api_base,
+            username,
+            password,
+            verify_ssl,
+            debug
+        )
 
-    if result['status'] in [200, 202]:
-        print('\033[1mStatus\033[0m: %(status)s' % result['body'])
-        print('\033[1mTimestamp\033[0m: %(performed_at)s' % result['body']['data'])
-        print('\n\033[1mOutput\033[0m:\n%(output)s' % result['body']['data'])
-    elif result['status'] in [400, 404]:
-        print('Server didn\'t like the argument. Please try with something meaningful.')
-        sys.exit(1)
-    elif result['status'] in [429]:
-        print(u'(╯°□°）╯︵ ┻━┻ Hey, what are you up to? Slow down!')
-        sys.exit(1)
-    elif result['status'] in [502]:
-        print('\033[1mStatus\033[0m: %(status)s' % result['body'])
-        print('\n\033[1mOutput\033[0m:\n%(message)s\n' % result['body']['data'])
-    elif result['status'] in [504]:
-        print(u'(╯°□°）╯︵ ┻━┻ Command timed out on the router')
-    else:
-        print(u'(╯°□°）╯︵ ┻━┻ Something went wrong with the server')
-
-    print('')
-
-def ping(router, address, vrf, max_routers, afi):
-    ctx = click.get_current_context()
-    try:
-        routers = ctx.meta['CLIENT'].get_routers(router)
-    except RuntimeError, error:
-        print(error)
-        sys.exit(1)
-
-    limit = len(routers) if len(routers) < max_routers else max_routers
-    _ = ', '.join(sorted([_['name'] for _ in routers[:limit]]))
-    print('\033[1mSelected routers\033[0m: %s\n' % _)
-
-    for _router in routers[:limit]:
-        print('\033[1mRouter\033[0m: %s' % _router['name'])
+    def print_result(self, result):
+        # TODO: Find a library for this
         try:
-            result = ctx.meta['CLIENT'].ping(address, _router['id'], afi, vrf)
+            _  = result['body']['data']['output']
+            _ = _.replace('<b>', '\033[1m')
+            _ = _.replace('</b>', '\033[0m')
+            result['body']['data']['output'] = _
+        except KeyError:
+            pass
+
+        if result['status'] in [200, 202]:
+            print('\033[1mStatus\033[0m: %(status)s' % result['body'])
+            print('\033[1mTimestamp\033[0m: %(performed_at)s' % result['body']['data'])
+            print('\n\033[1mOutput\033[0m:\n%(output)s' % result['body']['data'])
+        elif result['status'] in [400, 404]:
+            print('Server didn\'t like the argument. Please try with something meaningful.')
+            sys.exit(1)
+        elif result['status'] in [429]:
+            print(u'(╯°□°）╯︵ ┻━┻ Hey, what are you up to? Slow down!')
+            sys.exit(1)
+        elif result['status'] in [502]:
+            print('\033[1mStatus\033[0m: %(status)s' % result['body'])
+            print('\n\033[1mOutput\033[0m:\n%(message)s\n' % result['body']['data'])
+        elif result['status'] in [504]:
+            print(u'(╯°□°）╯︵ ┻━┻ Command timed out on the router')
+        else:
+            print(u'(╯°□°）╯︵ ┻━┻ Something went wrong with the server')
+
+        print('')
+
+
+    def ping(self, router, address, vrf, max_routers, afi):
+        try:
+            routers = self.show_routers(router, max_routers, True)
         except RuntimeError, error:
-            print(error)
+            print(unicode(error))
             sys.exit(1)
 
-        _print_result(result)
+        for _router in routers:
+            print('\033[1mRouter\033[0m: %s' % _router['name'])
+            try:
+                result = self.client.ping(address, _router['id'], afi, vrf)
+            except RuntimeError, error:
+                print(unicode(error))
+                sys.exit(1)
 
-def show_bgp_unicast(router, prefix, vrf, max_routers, afi):
-    ctx = click.get_current_context()
-    try:
-        routers = ctx.meta['CLIENT'].get_routers(router)
-    except RuntimeError, error:
-        print(error)
-        sys.exit(1)
+            self.print_result(result)
 
-    limit = len(routers) if len(routers) < max_routers else max_routers
-    _ = ', '.join(sorted([_['name'] for _ in routers[:limit]]))
-    print('\033[1mSelected routers\033[0m: %s\n' % _)
-
-    for _router in routers[:limit]:
-        print('\033[1mRouter\033[0m: %s' % _router['name'])
+    def show_bgp_unicast(self, router, prefix, vrf, max_routers, afi):
         try:
-            result = ctx.meta['CLIENT'].show_bgp_unicast(prefix, _router['id'], afi, vrf)
+            routers = self.show_routers(router, max_routers, True)
         except RuntimeError, error:
-            print(error)
+            print(unicode(error))
             sys.exit(1)
 
-        _print_result(result)
+        for _router in routers:
+            print('\033[1mRouter\033[0m: %s' % _router['name'])
+            try:
+                result = self.client.show_bgp_unicast(prefix, _router['id'], afi, vrf)
+            except RuntimeError, error:
+                print(unicode(error))
+                sys.exit(1)
 
-def show_routers(router):
-    ctx = click.get_current_context()
-    try:
-        result = ctx.meta['CLIENT'].show_routers(router)
-    except RuntimeError, error:
-        print(error)
-        sys.exit(1)
+            self.print_result(result)
 
-    print(result)
-
-def traceroute(router, address, vrf, max_routers, afi):
-    ctx = click.get_current_context()
-    try:
-        routers = ctx.meta['CLIENT'].get_routers(router)
-    except RuntimeError, error:
-        print(error)
-        sys.exit(1)
-
-    limit = len(routers) if len(routers) < max_routers else max_routers
-    _ = ', '.join(sorted([_['name'] for _ in routers[:limit]]))
-    print('\033[1mSelected routers\033[0m: %s\n' % _)
-
-    for _router in routers[:limit]:
-        print('\033[1mRouter\033[0m: %s' % _router['name'])
+    def show_routers(self, router, max_routers, compact=False):
         try:
-            result = ctx.meta['CLIENT'].traceroute(address, _router['id'], afi, vrf)
+            routers = self.client.get_routers(router)
         except RuntimeError, error:
-            print(error)
+            print(unicode(error))
             sys.exit(1)
 
-        _print_result(result)
+        if max_routers > 0:
+            limit = len(routers) if len(routers) < max_routers else max_routers
+            routers = sorted(routers[:limit], key=lambda _: _['name'])        
+
+        if compact:
+            router_names = ', '.join([_['name'] for _ in routers])
+            print('\033[1mSelected routers\033[0m: %s\n' % router_names)
+        else:
+            router_names = '\n'.join([_['name'] for _ in routers])
+            print(router_names)
+
+        return routers
+
+    def traceroute(self, router, address, vrf, max_routers, afi):
+        try:
+            routers = self.show_routers(router, max_routers, True)
+        except RuntimeError, error:
+            print(unicode(error))
+            sys.exit(1)
+
+        for _router in routers:
+            print('\033[1mRouter\033[0m: %s' % _router['name'])
+            try:
+                result = self.client.traceroute(address, _router['id'], afi, vrf)
+            except RuntimeError, error:
+                print(unicode(error))
+                sys.exit(1)
+
+            self.print_result(result)
