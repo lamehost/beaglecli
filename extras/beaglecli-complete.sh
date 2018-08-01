1a233da082eb4dd9df0ccfdc84a1713693adabab
Marco Marzetti <marco@lamehost.it>
2018-08-01 15:26:11 +0200

    regenerated 20180801

---
+++ extras/beaglecli-complete.sh
@@ -0,0 +1,8 @@
+_beaglecli_completion() {
+    COMPREPLY=( $( env COMP_WORDS="${COMP_WORDS[*]}" \
+                   COMP_CWORD=$COMP_CWORD \
+                   _BEAGLECLI_COMPLETE=complete $1 ) )
+    return 0
+}
+
+complete -F _beaglecli_completion -o default beaglecli;
